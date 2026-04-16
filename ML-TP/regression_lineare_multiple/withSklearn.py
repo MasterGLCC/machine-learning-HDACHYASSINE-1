dbccbff49bc5ccc5cbd7fac8448978b47b68fa73
HDACHYASSINE <smadara975@gmail.com>
2026-04-16 02:11:41 +0100

    Delete ML-TP directory

--- ML-TP/regression_lineare_multiple/withSklearn.py
+++
@@ -1,9 +0,0 @@
-from sklearn.linear_model import LinearRegression
-
-import Dataset
-
-model = LinearRegression()
-model.fit(Dataset.X, Dataset.y)
-
-print("Coefficients:", model.coef_)
-print("Intercept:", model.intercept_)
